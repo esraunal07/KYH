5b59843218b0a70f92397a6ca33c3cec09edb35a
Esra Unal <gesraunal@gmail.com>
2023-10-06 18:31:44 +0200

    update2.version

--- Husdjur/husdjur.js
+++ Husdjur/husdjur.js
@@ -1,5 +1,4 @@
 const prompt = require('prompt-sync')({ sigint: true });
-const question = require('./questions.json');
 const fs = require('fs');
 
 console.log("Välkommen! Du är här för det bästa valet!");
@@ -7,82 +6,92 @@ console.log("Välkommen! Du är här för det bästa valet!");
 const Användarnamn = prompt('Vad heter du?');
 const dagensdatum = new Date();
 const datumTid = dagensdatum.toString();
-console.log("Din starttid : ", datumTid);
-console.log("Svara på frågorna med ja/nej !");
+console.log("Din starttid: ", datumTid);
+console.log("Svara på frågorna med ja/nej!");
 
 function laddaQuestions() {
   try {
-    const data = fs.readFileSync("./questions.json");
-    return JSON.parse(data);
+    const data = fs.readFileSync("./questions.json", );
+    return JSON.parse(data).questions;
   } catch (error) {
     console.error("Ett fel uppstod när enkäten laddades:", error);
     return [];
   }
 }
 
-function saveAnswers(answers) {
+function saveAnswers(poäng) {
   try {
-    fs.writeFileSync('./answers.json', JSON.stringify(answers, null, 2));
+    fs.writeFileSync('./answers.json', JSON.stringify(poäng, null, 2));
   } catch (error) {
-    console.error('Failed to save results:', error);
+    console.error('Det gick inte att spara resultat:', error);
   }
 }
 
-const Uanswers = {
-    Katt: 0,
-    Hund: 0,
-    Kanin: 0,
-    Fisk: 0,
-  };
-  
-  const questions = laddaQuestions();
-  
-  for (let index = 0; index < question.questions.length; index++) {
-    let running = true;
-    while (running) {
-      const userAnswer = prompt(question.questions[index].question).toLowerCase();
-      if (userAnswer === "ja" || userAnswer === "nej") {
-        Uanswers[question.questions[index].animal] += userAnswer === "ja" ? 1 : 0;
-        running = false;
-      } else {
-        console.log("Något gick fel! Svara med 'ja' eller 'nej'.");
-      }
-    }   
+const poäng = {
+  katt: 0,
+  hund: 0,
+  kanin: 0,
+  fisk: 0,
+};
+
+const questions = laddaQuestions();
+
+for (let index = 0; index < questions.length; index++) {
+  let running = true;
+  while (running) {
+    const userAnswer = prompt(questions[index].question + ' (Ja/Nej)').toLowerCase();
+    if (userAnswer === "ja" || userAnswer === "nej") {
+      const animal = userAnswer === "ja" ? questions[index].poäng.Ja : questions[index].poäng.Nej;
+      Object.keys(animal).forEach((husdjur) => {
+        poäng[husdjur] += animal[husdjur];
+      });
+      running = false;
+    } else {
+      console.log("Något gick fel! Svara med 'ja' eller 'nej'.");
+    }
+  }
+}
+
+const husdjur = ['katt', 'hund', 'kanin', 'fisk'];
+
+for (const animal of husdjur) {
+  console.log(`${animal}: ${poäng[animal]} poäng`);
 }
 
 let totalpoäng = 0;
 
-for (const husdjur in Uanswers) {
-  totalpoäng += Uanswers[husdjur];
+for (const animal of husdjur) {
+  totalpoäng += poäng[animal];
 }
 
 const procentsatser = {};
 
-for (const husdjur in Uanswers) {
-  const procentsats = (Uanswers[husdjur] / question.questions.length) * 100; 
-  procentsatser[husdjur] = procentsats.toFixed(2);
+for (const animal of husdjur) {
+  const procentsats = (poäng[animal] / questions.length) * 100;
+  procentsatser[animal] = procentsats.toFixed(2);
 }
 
 let bästHusdjur = "";
 let högstaprocent = 0;
 
-for (const husdjur in procentsatser) {
-  if (parseFloat(procentsatser[husdjur]) > högstaprocent) {
-    högstaprocent = parseFloat(procentsatser[husdjur]);
-    bästHusdjur = husdjur;
+for (const animal of husdjur) {
+  if (parseFloat(procentsatser[animal]) > högstaprocent) {
+    högstaprocent = parseFloat(procentsatser[animal]);
+    bästHusdjur = animal;
   }
 }
 
 console.log(Användarnamn);
 console.log(dagensdatum);
 
-console.log("Baserat på dina preferenser är det bäst passande djuret : ", bästHusdjur);
+console.log("Baserat på dina preferenser är det bäst passande djuret: ", bästHusdjur);
 console.log("Procentuell matchning:", högstaprocent.toFixed(2) + "%");
 
 const userAnswers = [{
   namn: Användarnamn,
   date: dagensdatum,
-  result: `Baserat på dina preferenser är det bäst passande djuret : ${bästHusdjur}.`,
+  result: `Baserat på dina preferenser är det bäst passande djuret: ${bästHusdjur}.`,
 }];
 
 saveAnswers(userAnswers);
+

cbe9b7ba3810dbcdb3a38d4b2dd62b2162219685
Esra Unal <gesraunal@gmail.com>
2023-11-20 21:00:36 +0100

    20/11

--- BoardGame/ViewPlayers.js
+++ BoardGame/ViewPlayers.js
@@ -1,24 +1,21 @@
-document.addEventListener("DOMContentLoaded", function () {
-    // Sayfa yüklendiğinde çalışacak kodları buraya yazabilirsiniz.
-    displayPlayerList();
-  });
-  
-  function displayPlayerList() {
-    const playerListContainer = document.getElementById("playerList");
-  
-    // Oyuncu listesini local storage'tan al
-    const existingPlayers = JSON.parse(localStorage.getItem("players")) || {};
-  
-    // Her oyuncu için bir liste öğesi oluştur
-    Object.keys(existingPlayers).forEach(playerName => {
-      const playerItem = document.createElement("li");
-      playerItem.textContent = `Player: ${playerName}, Score: ${existingPlayers[playerName].score}`;
-      playerListContainer.appendChild(playerItem);
-    });
+document.addEventListener('DOMContentLoaded', function () {
+  const playerList = document.getElementById('playerList');
+
+  const storedPlayerXName = localStorage.getItem('playerXName');
+  const storedPlayerOName = localStorage.getItem('playerOName');
+
+  if (storedPlayerXName) {
+    addPlayerToList(storedPlayerXName);
   }
-  
-  function goToMainPage() {
-    // Ana sayfaya yönlendir
-    window.location.href = "main.html"; // Gitmek istediğiniz sayfanın dosya adını buraya ekleyin
+
+  if (storedPlayerOName) {
+    addPlayerToList(storedPlayerOName);
   }
-  
+});
+
+function addPlayerToList(playerName) {
+  const playerList = document.getElementById('playerList');
+  const listItem = document.createElement('li');
+  listItem.textContent = playerName;
+  playerList.appendChild(listItem);
+}
--- BoardGame/tictactoe.js
+++ BoardGame/tictactoe.js
@@ -12,80 +12,112 @@ const winningCombination = [
   // diag
   [0, 4, 8],
   [2, 4, 6]
-]
-const roundText = document.getElementById('round-text')
-const cellElements = document.querySelectorAll('[data-cell]')
-const winningMessageElement = document.getElementById('winning-message')
-const winningMessageTextElement = document.querySelector('[data-winning-message-text]')
-const resetButton = document.getElementById('restart-button')
+];
+
 let rounds = 1;
 let whoTurn;
 let markArray = [];
+let playerXName = "DefaultPlayer1";
+let playerOName = "DefaultPlayer2";
+let matchHistoryData = [];  
+const storedPlayerXName = localStorage.getItem('playerXName');
+const storedPlayerOName = localStorage.getItem('playerOName');
 
-startGame();
+if (storedPlayerXName) {
+  playerXName = storedPlayerXName;
+}
 
-resetButton.addEventListener('click', startGame)
+if (storedPlayerOName) {
+  playerOName = storedPlayerOName;
+}
+
+localStorage.setItem('playerXName', playerXName);
+console.log("Player X Name:", playerXName);
+localStorage.setItem('playerOName', playerOName);
+console.log("Player O Name:", playerOName);
+
+const roundText = document.getElementById('round-text');
+const cellElements = document.querySelectorAll('[data-cell]');
+const winningMessageTextElement = document.querySelector('[data-winning-message-text]');
+const resetButton = document.getElementById('restart-button');
+
+resetButton.addEventListener('click', startGame);
 
 function startGame() {
-
-  whoTurn = false
+  whoTurn = false;
   markArray = [];
   cellElements.forEach(cell => {
-    cell.classList.remove(X)
-    cell.classList.remove(O)
-    cell.innerText = ''
-    cell.removeEventListener('click', onClick)
-    cell.addEventListener('click', onClick)
-  })
-  roundText.innerHTML = `Round: ${rounds}`
-  winningMessageTextElement.innerText = ''
-  rounds++
+    cell.classList.remove(X);
+    cell.classList.remove(O);
+    cell.innerText = '';
+    cell.removeEventListener('click', onClick);
+    cell.addEventListener('click', onClick);
+  });
+  roundText.innerHTML = `Round: ${rounds}`;
+  winningMessageTextElement.innerText = '';
+  rounds++;
   if (rounds > 3) {
-    rounds = 1
+    rounds = 1;
   }
 }
 
 function onClick(e) {
-  const cell = e.target
+  const cell = e.target;
   const currentClass = whoTurn ? O : X;
   if (checkCellEmpty(cell) && !checkWin(currentClass)) {
-    cell.innerText = whoTurn ? O : X
-    placeMarke(cell, currentClass)
+    cell.innerText = whoTurn ? O : X;
+    placeMarke(cell, currentClass);
   }
 }
 
 function checkCellEmpty(cell) {
   if (cell.classList.contains("x") || cell.classList.contains("o")) {
-    return false
+    return false;
   }
-  return true
+  return true;
 }
 
 function endGame() {
-  winningMessageTextElement.innerText = `${whoTurn ? "O" : "X"} Wins!`
+  winningMessageTextElement.innerText = `${whoTurn ? playerOName : playerXName} Wins!`;
+  showSelectedPlayers();
+  matchHistoryData.push({
+    winner: whoTurn ? playerOName : playerXName,
+    opponent: whoTurn ? playerXName : playerOName,
+    roundCount: rounds - 1,
+    result: whoTurn ? "O Wins" : "X Wins" 
+  });
+
+  localStorage.setItem('matchHistoryData', JSON.stringify(matchHistoryData));
+}
+
+function showSelectedPlayers() {
+  const selectedPlayersDiv = document.getElementById('selectedPlayers');
+  selectedPlayersDiv.innerHTML = `<p>${playerXName} vs ${playerOName}</p>`;
 }
 
 function placeMarke(cell, currentClass) {
-  markArray.push(cell)
+  markArray.push(cell);
   cell.classList.add(currentClass);
   if (checkWin(currentClass)) {
-    endGame()
+    endGame();
   } else {
     if (markArray.length === 9) {
-      winningMessageTextElement.innerText = 'Tie!'
+      winningMessageTextElement.innerText = 'Tie!';
     }
-    swapTurns()
+    swapTurns();
   }
 }
 
 function swapTurns() {
-  whoTurn = !whoTurn
+  whoTurn = !whoTurn;
 }
 
 function checkWin(currentClass) {
-  return winningCombination.some(cobination => {
-    return cobination.every(index => {
-      return cellElements[index].classList.contains(currentClass)
-    })
-  })
+  return winningCombination.some(combination => {
+    return combination.every(index => {
+      return cellElements[index].classList.contains(currentClass);
+    });
+  });
 }
+
+startGame();
